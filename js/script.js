1d27139435f8b3907591651917acbb036af28314
Md. Yeakub <mdyeakub.cse@gmail.com>
2023-03-03 21:30:39 +0600

    new comments

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// =======typing animation========
+var typed = new Typed(".typing", {
+    strings:["","Python And Django Developer","Web Designer", "Web Developer","Data Engineer","Software Developer"],
+    typeSpeed:80,
+    BackSpeed:60,
+    loop:true
+})
